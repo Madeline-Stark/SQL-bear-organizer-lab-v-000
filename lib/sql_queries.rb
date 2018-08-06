9ced9c98e500311dff85caf0f72a6ced3dfb6952
Madeline Stark <madeline.stark1@gmail.com>
2018-08-06 00:38:26 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,9 +28,10 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT      `temperament`, WHERE temperament = 'goofy'
+  "SELECT      `temperament`,
                COUNT(`temperament`) AS `value_occurrence`
       FROM     `bears`
+      WHERE temperament = 'goofy'
       GROUP BY `temperament`
       ORDER BY `value_occurrence` DESC
       LIMIT    1";
